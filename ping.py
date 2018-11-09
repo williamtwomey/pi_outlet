e91a9d9d42dd3e7c8fab99d425a032f86ec09f4e
will7925 <will.twomey@RACKSPACE.COM>
2018-11-09 00:37:07 -0600

    v0.1

--- ping.py
+++ ping.py
@@ -7,8 +7,6 @@ from threading import Thread
 hosts = [ '8.8.8.8', '4.2.2.2', '8.8.8.91' ]
 trigger = "False"
 
-sem = BoundedSemaphore(len(hosts))
-
 def ping(i):
     code = os.system('ping -c 3 ' + i + ' >/dev/null')
     print("thread exited with code ", code)
