ac67e0342b7d49a629453579300103f2b5755a41
will7925 <will.twomey@RACKSPACE.COM>
2018-11-09 00:36:17 -0600

    v0.1

---
+++ ping.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+import time
+from threading import Thread
+
+hosts = [ '8.8.8.8', '4.2.2.2', '8.8.8.91' ]
+trigger = "False"
+
+sem = BoundedSemaphore(len(hosts))
+
+def ping(i):
+    code = os.system('ping -c 3 ' + i + ' >/dev/null')
+    print("thread exited with code ", code)
+    if code > 0:
+        global trigger
+        trigger = "True"
+
+
+for i in hosts:
+    t = Thread(target=ping, args=(i,))
+    t.start()
+    t.join()
+
+print("main script")
+print("trigger: ", trigger)
