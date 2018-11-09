506772e00cfa69fd9a8058525e637fc0a05cd78c
will7925 <will.twomey@RACKSPACE.COM>
2018-11-09 15:30:02 -0600

    ping.py

--- ping.py
+++ ping.py
@@ -10,21 +10,22 @@ LOOP_TIME = "10"
 #number of fails before we activate relay
 MAX_FAIL = 3
 
-# LOOP_TIME * MAX_FAIL = Time to detect failure and trigger relay
+# LOOP_TIME * MAX_FAIL = Time to detect FAILure and trigger relay
 
-#hosts to ping
-hosts = [ '8.8.8.8', '4.2.2.2', '1.1.1.1' ]
+#HOSTS to ping
+HOSTS = [ '8.8.8.8', '4.2.2.2', '1.1.1.1' ]
 
 #host local to us that should always be up (generally default gateway)
+#used to prevent false positives
 #otherwise just use localhost (127.0.0.1)
-monitor = "127.0.0.1"
+MONITOR = "127.0.0.1"
 
-#percent of hosts that can be down before we consider it a failure
+#percent of HOSTS that can be DOWN before we consider it a failure
 #1=100%, .5=50%, etc.
-threshold = 1
+THRESHOLD = 1
 
-fail = 0
-down = 0
+FAIL = 0
+DOWN = 0
 
 #GPIO pin
 GPIO = 7
@@ -33,35 +34,35 @@ def ping(i):
     code = os.system('ping -c 3 ' + i + ' >/dev/null')
     print(i," ", code)
     if code > 0:
-        global down
-        down += 1
+        global DOWN
+        DOWN += 1
 
 #Main loop
 while True:
-        #ensure our monitor IP is up, otherwise no point in continuing
-        if os.system('ping -c 3 ' + monitor + ' >/dev/null') > 0:
-            #Our monitor IP is down, so we should do nothing
-            print("monitor ip ", monitor, " is down")
-            time.sleep(1)
+        #ensure our MONITOR IP is up, otherwise no point in continuing
+        if os.system('ping -c 3 ' + MONITOR + ' >/dev/null') > 0:
+            #Our MONITOR IP is DOWN, so we should do nothing
+            print("MONITOR ip ", MONITOR, " is DOWN")
+            time.sleep(10)
         else:
             #start ping threads
-            for i in hosts:
+            for i in HOSTS:
                 t = Thread(target=ping, args=(i,))
                 t.start()
             #ensure all threads have finished
             t.join()
 
-            #determine % of failed hosts
-            p = down / len(hosts)
-            if p >= threshold:
-                fail += 1
+            #determine % of failed HOSTS
+            p = DOWN / len(HOSTS)
+            if p >= THRESHOLD:
+                FAIL += 1
 
-            down = 0
+            DOWN = 0
 
             print("percent failed: ", p)
-            print("fails: ", fail)
+            print("fails: ", FAIL)
 
-            if fail == MAX_FAIL:
+            if FAIL == MAX_FAIL:
                 print("max fails reached")
                 #os.system('gpio write 7 1') #else we get a brief power flicker when changing the mode
                 #os.system('gpio mode 7 out')
@@ -70,7 +71,7 @@ while True:
                 #time.sleep(30)
                 #os.system('gpio write 7 1') #turn it back on
                 #time.sleep(300) #sleep for 5min to give it a chance
-                fail = 0
+                FAIL = 0
      
             #wait 30s to run main loop again
             time.sleep(int(LOOP_TIME))
