3a1af036fab0032caedcadd04c96d5ed5aa64037
will7925 <will.twomey@RACKSPACE.COM>
2018-11-09 15:24:42 -0600

    ping.py

--- ping.py
+++ ping.py
@@ -4,15 +4,24 @@ import os
 import time
 from threading import Thread
 
-#hosts to ping
-hosts = [ '8.8.8.8', '4.2.2.2', '1.2.3.4' ]
-
+#How often we should ping (s)
+LOOP_TIME = "10"
 
 #number of fails before we activate relay
-max_fail = 3
+MAX_FAIL = 3
+
+# LOOP_TIME * MAX_FAIL = Time to detect failure and trigger relay
+
+#hosts to ping
+hosts = [ '8.8.8.8', '4.2.2.2', '1.1.1.1' ]
+
+#host local to us that should always be up (generally default gateway)
+#otherwise just use localhost (127.0.0.1)
+monitor = "127.0.0.1"
 
 #percent of hosts that can be down before we consider it a failure
-threshold = .3
+#1=100%, .5=50%, etc.
+threshold = 1
 
 fail = 0
 down = 0
@@ -27,36 +36,41 @@ def ping(i):
         global down
         down += 1
 
+#Main loop
 while True:
-        for i in hosts:
-            t = Thread(target=ping, args=(i,))
-            t.start()
+        #ensure our monitor IP is up, otherwise no point in continuing
+        if os.system('ping -c 3 ' + monitor + ' >/dev/null') > 0:
+            #Our monitor IP is down, so we should do nothing
+            print("monitor ip ", monitor, " is down")
+            time.sleep(1)
+        else:
+            #start ping threads
+            for i in hosts:
+                t = Thread(target=ping, args=(i,))
+                t.start()
+            #ensure all threads have finished
+            t.join()
 
+            #determine % of failed hosts
+            p = down / len(hosts)
+            if p >= threshold:
+                fail += 1
 
-	#ensure all threads have finished
-        t.join()
+            down = 0
 
+            print("percent failed: ", p)
+            print("fails: ", fail)
 
-        p = down / len(hosts)
-        if p >= threshold:
-            fail += 1
-
-        down = 0
-
-        print("percent failed: ", p)
-        print("fails: ", fail)
-
-        if fail == max_fail:
-            print("max fails reached")
-            #trigger relay, sleep for something crazy like 5min
-            #os.system('gpio write 7 1') #else we get a brief power flicker before the actual reset
-            #os.system('gpio mode 7 out')
-            #os.system('gpio write 7 0')
-            #Power off for 30 seconds
-            #time.sleep(30)
-            #os.system('gpio write 7 1') #turn it back on
-            #time.sleep(300) #sleep for 5min to give it a chance
-            fail = 0
-
-	#wait 30s to run main loop again
-        time.sleep(3)
+            if fail == MAX_FAIL:
+                print("max fails reached")
+                #os.system('gpio write 7 1') #else we get a brief power flicker when changing the mode
+                #os.system('gpio mode 7 out')
+                #os.system('gpio write 7 0')
+                #Power off for 30 seconds
+                #time.sleep(30)
+                #os.system('gpio write 7 1') #turn it back on
+                #time.sleep(300) #sleep for 5min to give it a chance
+                fail = 0
+     
+            #wait 30s to run main loop again
+            time.sleep(int(LOOP_TIME))
